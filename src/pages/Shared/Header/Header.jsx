100f181357decbf1cb8ac2431e1541474d340f5e
mehedihasanrupok <hasanmehedi4114@gmail.com>
2023-05-19 00:19:54 +0600

    Navbar Done

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -4,20 +4,24 @@ import logo from '../../../../public/2.jpg'
 import './Header.css'
 
 const Header = () => {
-    
-    return (
-       <div>
-          <div className='p-3'>
-             <img className='my-logo' src={logo} alt="" />
-             <h3 className='text-orange-500 italic font-bold text-3xl'>Baby Toy</h3>
-          </div>
-          <div>
-            <Link></Link>
-          </div>
-          <div>
 
-          </div>
-       </div>
+    return (
+        <div className='flex justify-between font-bold items-center'>
+            <div className='p-3'>
+                <img className='my-logo' src={logo} alt="" />
+                <h3 className='text-orange-500 italic font-bold text-3xl'>Baby Toy</h3>
+            </div>
+            <div className='p-4 space-x-5 items-center'>
+                <Link to='/'>Home</Link>
+                <Link>My Toys</Link>
+                <Link>All Toys</Link>
+                <Link>Blogs</Link>
+            </div>
+            <div className='p-4 justify-around space-x-2.5'>
+                <Link>Login</Link>
+                <button>MyPicture</button>
+            </div>
+        </div>
     );
 };
 
