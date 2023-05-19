b85cd328699a418db826cdf9df712d2f02faf4dc
mehedihasanrupok <hasanmehedi4114@gmail.com>
2023-05-19 23:36:40 +0600

    Banner Added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -3,13 +3,18 @@ import { Outlet } from 'react-router-dom';
 import Footer from '../pages/Shared/Footer/Footer';
 import Header from '../pages/Shared/Header/Header';
 
+
+// window.location.pathname !== '/login' ? 
+
 const Main = () => {
+
     return (
         <div>
             <Header></Header>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
+
     );
 };
 
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -25,6 +25,10 @@ const router = createBrowserRouter([
         {
             path:'/login',
             element:<Login></Login>
+        },
+        {
+            path:'*',
+            element:<Error></Error>
         }
       ]
     },
