93458b46f61074ad152d570cee14019a77ebe25b
mehedihasanrupok <hasanmehedi4114@gmail.com>
2023-05-20 21:24:48 +0600

    Error Page without header footer

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -7,42 +7,44 @@ import Login from "../pages/LogIn/Login";
 import MyToy from "../pages/MyToy/MyToy";
 import AddToy from "../pages/AddToy/AddToy";
 import PrivateRoute from "./PrivateRoute";
+import Error from "../pages/Error/Error";
 
 const router = createBrowserRouter([
     {
-      path: "/",
-      element: <Main></Main>,
-      children: [
+        path: "/",
+        element: <Main></Main>,
+        errorElement:<Error></Error>,
+        children: [
         {
-            path:'/',
-            element:<Home></Home>
+            path: '/',
+            element: <Home></Home>
         },
         {
-            path:'/blogs',
-            element:<Blogs></Blogs>
+            path: '/blogs',
+            element: <Blogs></Blogs>
         },
         {
-            path:'/signup',
-            element:<SignIn></SignIn>
+            path: '/signup',
+            element: <SignIn></SignIn>
         },
         {
-            path:'/login',
-            element:<Login></Login>
+            path: '/login',
+            element: <Login></Login>
+        },
+        // {
+        //     path: '*',
+        //     element: <Error></Error>
+        // },
+        {
+            path: '/mytoy',
+            element: <PrivateRoute><MyToy></MyToy></PrivateRoute>
         },
         {
-            path:'*',
-            element:<Error></Error>
-        },
-        {
-            path:'/mytoy',
-            element:<PrivateRoute><MyToy></MyToy></PrivateRoute>
-        },
-        {
-            path:'/addtoy',
-            element:<PrivateRoute><AddToy></AddToy></PrivateRoute>
+            path: '/addtoy',
+            element: <PrivateRoute><AddToy></AddToy></PrivateRoute>
         }
-      ]
+    ]
     },
-  ]);
+]);
 
-  export default router;
+export default router;
--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import img from '../../../public/error.jpg'
+import { Link } from 'react-router-dom';
 
 const Error = () => {
     return (
         <div>
-            <h1>This is Error Page</h1>
+            <div className='relative'>
+                <img className='w-9/12 h-min' src={img} alt="" />
+                <div className='absolute top-0 left-5'>
+                   <button className='text-black text-5xl rounded-md m-3 bg-slate-500'><Link to='/'>Go To Home</Link></button>
+                </div>
+            </div>
         </div>
     );
 };
